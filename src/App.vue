45517bec5b8ea6b404ff41f5935f4a439de33561
Kos Max <maxie7@protonmail.com>
2017-08-27 22:03:45 +0200

    Initial commit with VueComponents

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+
+   <p>Server Status: {{ status }} </p>
+
+</template>
+
+<script>
+    export default {
+        data: function () {
+          return {
+            status: 'Critical'
+          }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
