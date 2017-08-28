cc7516b5f31be709a75efc93f5ae39e6baefc94b
Kos Max <maxie7@protonmail.com>
2017-08-28 19:54:03 +0200

    Refactor & add validation

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,10 +2,28 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+        <button @click="ResetName">Reset Name</button>
     </div>
 </template>
 
 <script>
+    export default {
+        props: {
+            myName: {
+                type: String,
+                required: true
+            }
+        },
+        methods: {
+            switchName() {
+                return this.myName.split("").reverse().join("");
+            },
+            ResetName() {
+                
+            }
+        }
+    }
 </script>
 
 <style scoped>
