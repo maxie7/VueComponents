a31c50a7189e84be67ab6e3dacdeeea4205200d8
Kos Max <maxie7@protonmail.com>
2017-08-29 14:56:13 +0200

    Add eventBus

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+    import { eventBus } from '../main';
+
     export default {
         props: {
             myName: {
@@ -27,6 +29,11 @@
                 this.myName = 'Max';
                 this.$emit('nameWasReset', this.myName);
             }
+        },
+        created() {
+            eventBus.$on('ageWasEdited', (age) => {
+                this.userAge = age;
+            });
         }
     }
 </script>
